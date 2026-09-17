5207bbc7939af1dee49f35514c393e8fce7826ae
xiaodong <2304334266@qq.com>
2026-09-17 10:50:36 +0800

    调整id为宏实现及其头文件

--- User/main.c
+++ User/main.c
@@ -1,15 +1,15 @@
-#include "Config.h"
+#include "App_Public.h"
 
 void sys_init(void) {
     
 }
 
 // 这里函数名可随意, 建议不要使用start, 会和I2C.h里的Start冲突
-void main_start() _task_ 0 {
+void main_start() _task_ App_Main_Task_Id {
 	sys_init();
 	// 创建任务 1
 	os_create_task(1);
 	// 结束任务 0
-    os_create_task(2);
+    os_create_task(App_Oscilloscope_Task_Id);
 	os_delete_task(0);
 }
